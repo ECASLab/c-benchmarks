25814b276a8aceefb9ce85a4d03cb5f7518a36b1
Diego <dandida95@gmail.com>
2025-02-27 00:34:45 -0600

    Add support for precommit hooks
    Added pre-commit hook support

--- include/md5.h
+++ include/md5.h
@@ -1,18 +1,20 @@
 /*
  * This file is part of the C Benchmarks project.
- * 
- * Licensed under the MIT License. See LICENSE file in the project root for full license information.
- * 
- * Authors: 
+ *
+ * Licensed under the GPL v2.1 License. 
+ * See LICENSE file in the project root for full
+ * license information.
+ *
+ * Authors:
  * Diego Avila <dandida95@gmail.com>
  * Luis G. Leon Vega <luis.leon@ieee.org>
+ *
  */
- 
+
 #pragma once
 
 #include <stdint.h>
 #include <string.h>
-
 
 unsigned int leftRotate(unsigned int x, unsigned int n);
 
