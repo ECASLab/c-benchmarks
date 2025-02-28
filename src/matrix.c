71b0ad48afcacbe8383f1166e218c9170c2251a9
Diego <dandida95@gmail.com>
2025-02-27 20:28:54 -0600

    Add matrix multiplication method
    The int matrix multiplication method
    was added in the header folder and its implemented in src

--- src/matrix.c
+++ src/matrix.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of the C Benchmarks project.
  *
- * Licensed under the GPL v2.1 License. 
+ * Licensed under the GPL v2.1 License.
  * See LICENSE file in the project root for full
  * license information.
  *
@@ -11,10 +11,11 @@
  *
  */
 
+#include "include/matrix.h"
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "include/matrix.h"
+
 #include "include/status.h"
 
 status int_matrix_alloc(int_matrix **input, size_t rows, size_t columns) {
@@ -53,7 +54,6 @@ status int_matrix_alloc(int_matrix **input, size_t rows, size_t columns) {
 }
 
 status int_matrix_free(int_matrix **input) {
-
   status output = {0, "OK"};
 
   if (input == NULL) {
@@ -92,6 +92,45 @@ status int_matrix_sum(int_matrix **input1, int_matrix **input2,
   for (size_t i = 0; i < (*output)->rows; i++) {
     for (size_t j = 0; j < (*output)->columns; j++) {
       (*output)->data[i][j] = (*input1)->data[i][j] + (*input2)->data[i][j];
+    }
+  }
+
+  return returned;
+}
+
+status int_matrix_multiplication(int_matrix **input1, int_matrix **input2,
+                                 int_matrix **output) {
+  status returned = {0, "OK \n"};
+
+  if (*input1 == NULL || *input2 == NULL || *output == NULL) {
+    returned.code = -1;
+    returned.message = "All the int_matrix pointers have to be allocated. \n";
+    return returned;
+  }
+
+  if ((*input1)->columns != (*input2)->rows) {
+    returned.message =
+      "Number of columns of the first matrix must equal the number of rows of "
+      "the second matrix. \n";
+    returned.code = -1;
+    return returned;
+  }
+
+  if ((*output)->rows != (*input1)->rows ||
+      (*output)->columns != (*input2)->columns) {
+    returned.message =
+      "Output matrix dimensions must match the result of the multiplication. "
+      "\n";
+    returned.code = -1;
+    return returned;
+  }
+
+  for (size_t i = 0; i < (*output)->rows; i++) {
+    for (size_t j = 0; j < (*output)->columns; j++) {
+      (*output)->data[i][j] = 0;
+      for (size_t k = 0; k < (*input1)->columns; k++) {
+        (*output)->data[i][j] += (*input1)->data[i][k] * (*input2)->data[k][j];
+      }
     }
   }
 
